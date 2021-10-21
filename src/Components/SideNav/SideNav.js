7413f61325169a7810d0a4ed4bdfbaedef6ba45e
ben-dass <benrdass@gmail.com>
2021-10-20 21:11:47 -0600

    fixed some styling issues

--- src/Components/SideNav/SideNav.js
+++ src/Components/SideNav/SideNav.js
@@ -1,21 +1,38 @@
 import React from "react";
-import { GrAdd } from 'react-icons/gr';
+import { AiOutlinePlus } from "react-icons/ai";
 
 import classes from "./SideNav.module.scss";
 
 const SideNav = () => {
-    // TODO: Add connection to Firebase. Collection of Categories, ...
-    // TODO: ... each account will always start with a 'General' ...
-    // TODO: ... category. There will be 2 collections: 'categories' and ...
-    // TODO: ... 'images'. Each Image will have an additional property ...
-    // TODO: ... array of strings 'tags', of its associated tags.
+  // TODO: Add connection to Firebase. Collection of Categories, ...
+  // TODO: ... each account will always start with a 'General' ...
+  // TODO: ... category. There will be 2 collections: 'categories' and ...
+  // TODO: ... 'images'. Each Image will have an additional property ...
+  // TODO: ... array of strings 'tags', of its associated tags.
 
   return (
     <div className={classes.sideNav}>
       <div className={classes.sideNavContainer}>
+        {/* Site title */}
         <div className={classes.sideNavTitle}>Sorted</div>
-      </div>
-      <div className={classes.sideNavList}>
+
+        {/* Categories */}
+        <div className={classes.sideNavList}>
+          <div className={classes.sideNavListTitle}>categories</div>
+
+          {/* List of categories */}
+          <ul className={classes.list}>
+            <li className={classes.listItem}>General</li>
+            <li className={classes.listItem}>Wallpapers</li>
+            <li className={classes.listItem}>Trips</li>
+          </ul>
+        </div>
+
+        {/* Add a category */}
+        <div className={classes.add}>
+          <AiOutlinePlus className={classes.icon} />
+          &nbsp; Add category &nbsp;&nbsp;
+        </div>
       </div>
     </div>
   );
