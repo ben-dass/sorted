9195dd10623e8b6e915816e1b0195c697bfd3e50
ben-dass <benrdass@gmail.com>
2021-11-05 12:36:41 -0600

    added settings icon

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,18 +1,20 @@
 import React from "react";
+import { IoMdSettings } from "react-icons/io";
 
 import classes from "./Content.module.scss";
 
 const Content = (props) => {
-  const category = props.category;
+	const category = props.category;
 
-  return (
-    <div className={classes.contentContainer}>
-      <div className={classes.titleBar}>
-        <div className={classes.title}>{category}</div>
-      </div>
-      <div className={classes.content}></div>
-    </div>
-  );
+	return (
+		<div className={classes.contentContainer}>
+			<div className={classes.titleBar}>
+				<div className={classes.title}>{category}</div>
+				<IoMdSettings className={classes.icon} />
+			</div>
+			<div className={classes.content}></div>
+		</div>
+	);
 };
 
 export default Content;
